844f08b244c53dfdd24100e51e6e0b6732fa004b
Roberto Kang <roberto.kang@bolttech.io>
2022-10-07 12:12:47 -0300

    refactor: change test strategy to use jest mock extended

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,11 +1,12 @@
+import { ILoadFacebookUserApi } from '@/data/contracts/apis'
 import { FacebookAuthenticationService } from '@/data/services'
 import { AuthenticationError } from '@/domain/errors'
 
+import { mock } from 'jest-mock-extended'
+
 describe('FacebookAuthenticationService', () => {
   it('Should call ILoadFacebookUserApi with correct params', async () => {
-    const loadFacebookUserApi = {
-      loadUser: jest.fn()
-    }
+    const loadFacebookUserApi = mock<ILoadFacebookUserApi>()
     const sut = new FacebookAuthenticationService(loadFacebookUserApi)
     await sut.perform({ token: 'any_token' })
 
@@ -14,9 +15,7 @@ describe('FacebookAuthenticationService', () => {
   })
 
   it('Should return AuthenticationError when ILoadFacebookUserApi returns undefined', async () => {
-    const loadFacebookUserApi = {
-      loadUser: jest.fn()
-    }
+    const loadFacebookUserApi = mock<ILoadFacebookUserApi>()
     loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined)
     const sut = new FacebookAuthenticationService(loadFacebookUserApi)
 
