58efb87ec1863a52f60f8bbd702db67456d7d613
Roberto Kang <roberto.kang@bolttech.io>
2022-10-07 00:10:14 -0300

    test: ensure Service return AuthenticationError when ILoadFacebookUserApi returns undefined

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,3 +1,4 @@
+import { AuthenticationError } from '@/domain/errors'
 import { IFacebookAuthentication } from '@/domain/features'
 
 class FacebookAuthenticationService {
@@ -5,26 +6,31 @@ class FacebookAuthenticationService {
     private readonly loadFacebookUserApi: ILoadFacebookUserApi
   ) {}
 
-  async perform (params: IFacebookAuthentication.Params): Promise<void> {
+  async perform (params: IFacebookAuthentication.Params): Promise<AuthenticationError> {
     await this.loadFacebookUserApi.loadUser(params)
+    return new AuthenticationError()
   }
 }
 
 interface ILoadFacebookUserApi {
-  loadUser: (params: ILoadFacebookUserApi.Params) => Promise<void>
+  loadUser: (params: ILoadFacebookUserApi.Params) => Promise<ILoadFacebookUserApi.Result>
 }
 
 namespace ILoadFacebookUserApi {
   export type Params = {
     token: string
   }
+
+  export type Result = undefined
 }
 
 class ILoadFacebookUserApiSpy implements ILoadFacebookUserApi {
   token?: string
+  result = undefined
 
-  async loadUser (params: ILoadFacebookUserApi.Params): Promise<void> {
+  async loadUser (params: ILoadFacebookUserApi.Params): Promise<ILoadFacebookUserApi.Result> {
     this.token = params.token
+    return this.result
   }
 }
 
@@ -35,5 +41,15 @@ describe('FacebookAuthenticationService', () => {
     await sut.perform({ token: 'any_token' })
 
     expect(loadFacebookUserApi.token).toBe('any_token')
+  })
+
+  it('Should return AuthenticationError when ILoadFacebookUserApi returns undefined', async () => {
+    const loadFacebookUserApi = new ILoadFacebookUserApiSpy()
+    loadFacebookUserApi.result = undefined
+    const sut = new FacebookAuthenticationService(loadFacebookUserApi)
+
+    const authResult = await sut.perform({ token: 'any_token' })
+
+    expect(authResult).toEqual(new AuthenticationError())
   })
 })
