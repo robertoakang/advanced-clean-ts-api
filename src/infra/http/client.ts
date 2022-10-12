f617a42dba659893ddb33bc2c78e754095663a86
Roberto Kang <roberto.kang@bolttech.io>
2022-10-12 09:56:48 -0300

    test: ensure FacebookApi gets debug token

--- src/infra/http/client.ts
+++ src/infra/http/client.ts
@@ -1,9 +1,11 @@
 export interface IHttpGetClient {
-  get: (params: IHttpGetClient.Params) => Promise<void>
+  get: (params: IHttpGetClient.Params) => Promise<IHttpGetClient.Result>
 }
 export namespace IHttpGetClient {
   export type Params = {
     url: string
     params: object
   }
+
+  export type Result = any
 }
