2ef8c273df036cca6560a224c2e3e8525dcca3e5
Roberto Kang <roberto.kang@bolttech.io>
2022-10-12 09:37:43 -0300

    refactor: move sut creation to beforeEach

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -1,6 +1,6 @@
 import { ILoadFacebookUserApi } from '@/data/contracts/apis'
 
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
 
 class FacebookApi {
   private readonly baseUrl: string = 'https://graph.facebook.com'
@@ -34,13 +34,22 @@ namespace IHttpGetClient {
 }
 
 describe('FacebookApi', () => {
-  const clientId = 'any_client_id'
-  const clientSecret = 'any_client_secret_id'
+  let sut: FacebookApi
+  let httpClient: MockProxy<IHttpGetClient>
+  let clientId: string
+  let clientSecret: string
+
+  beforeAll(() => {
+    clientId = 'any_client_id'
+    clientSecret = 'any_client_secret_id'
+    httpClient = mock()
+  })
+
+  beforeEach(() => {
+    sut = new FacebookApi(httpClient, clientId, clientSecret)
+  })
 
   it('Should get app token', async () => {
-    const httpClient = mock<IHttpGetClient>()
-    const sut = new FacebookApi(httpClient, clientId, clientSecret)
-
     await sut.loadUser({ token: 'any_client_token' })
 
     expect(httpClient.get).toHaveBeenCalledWith({
