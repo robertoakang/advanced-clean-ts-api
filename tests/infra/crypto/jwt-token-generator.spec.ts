21c0dae2c7205404c1865fbe0b2a450b377690f9
Roberto Kang <roberto.kang@bolttech.io>
2022-10-13 12:52:41 -0300

    refactor: isolate sut creation

--- tests/infra/crypto/jwt-token-generator.spec.ts
+++ tests/infra/crypto/jwt-token-generator.spec.ts
@@ -14,9 +14,18 @@ class JwtTokenGenerator {
 }
 
 describe('JwtTokenGenerator', () => {
+  let sut: JwtTokenGenerator
+  let fakeJwt: jest.Mocked<typeof jwt>
+
+  beforeAll(() => {
+    fakeJwt = jwt as jest.Mocked<typeof jwt>
+  })
+
+  beforeEach(() => {
+    sut = new JwtTokenGenerator('any_secret')
+  })
+
   it('Should call sign with correct params', async () => {
-    const fakeJwt = jwt as jest.Mocked<typeof jwt>
-    const sut = new JwtTokenGenerator('any_secret')
     await sut.generateToken({ key: 'any_key', expirationInMs: 1000 })
 
     expect(fakeJwt.sign).toHaveBeenCalledWith({ key: 'any_key' }, 'any_secret', { expiresIn: 1 })
