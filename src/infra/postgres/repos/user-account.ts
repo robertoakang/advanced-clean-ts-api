31382d6e878ad0d25a3cf9e632ea73810a22075e
Roberto Kang <roberto.kang@bolttech.io>
2022-10-14 11:21:40 -0300

    test: ensure PgUserAccountRepository creates an account if id is undefined

--- src/infra/postgres/repos/user-account.ts
+++ src/infra/postgres/repos/user-account.ts
@@ -1,4 +1,4 @@
-import { ILoadUserAccountRepository } from '@/data/contracts/repos'
+import { ILoadUserAccountRepository, ISaveFacebookAccountRepository } from '@/data/contracts/repos'
 import { PgUser } from '@/infra/postgres/entities'
 
 import { getRepository } from 'typeorm'
@@ -8,9 +8,18 @@ export class PgUserAccountRepository implements ILoadUserAccountRepository {
     const pgUser = await pgUserRepo.findOne({ email: params.email })
     if (pgUser !== undefined) {
       return {
-        id: pgUser?.id.toString(),
+        id: pgUser.id.toString(),
         name: pgUser.name ?? undefined
       }
     }
+  }
+
+  async saveWithFacebook (params: ISaveFacebookAccountRepository.Params): Promise<void> {
+    const pgUserRepo = getRepository(PgUser)
+    await pgUserRepo.save({
+      email: params.email,
+      name: params.name,
+      facebookId: params.facebookId
+    })
   }
 }
