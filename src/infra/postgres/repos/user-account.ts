a84aefe3588fd30deefdfbca3cc303fa6b2d1c39
Roberto Kang <roberto.kang@bolttech.io>
2022-10-14 11:09:34 -0300

    refactor: move production code to their own files

---
+++ src/infra/postgres/repos/user-account.ts
@@ -0,0 +1,16 @@
+import { ILoadUserAccountRepository } from '@/data/contracts/repos'
+import { PgUser } from '@/infra/postgres/entities'
+
+import { getRepository } from 'typeorm'
+export class PgUserAccountRepository implements ILoadUserAccountRepository {
+  async load (params: ILoadUserAccountRepository.Params): Promise<ILoadUserAccountRepository.Result> {
+    const pgUserRepo = getRepository(PgUser)
+    const pgUser = await pgUserRepo.findOne({ email: params.email })
+    if (pgUser !== undefined) {
+      return {
+        id: pgUser?.id.toString(),
+        name: pgUser.name ?? undefined
+      }
+    }
+  }
+}
--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -1,34 +1,18 @@
-import { ILoadUserAccountRepository } from '@/data/contracts/repos'
+import { PgUserAccountRepository } from '@/infra/postgres/repos'
+import { PgUser } from '@/infra/postgres/entities'
 
-import { IBackup, newDb } from 'pg-mem'
-import { Column, Entity, getConnection, getRepository, PrimaryGeneratedColumn, Repository } from 'typeorm'
+import { IBackup, IMemoryDb, newDb } from 'pg-mem'
+import { getConnection, getRepository, Repository } from 'typeorm'
 
-class PgUserAccountRepository implements ILoadUserAccountRepository {
-  async load (params: ILoadUserAccountRepository.Params): Promise<ILoadUserAccountRepository.Result> {
-    const pgUserRepo = getRepository(PgUser)
-    const pgUser = await pgUserRepo.findOne({ email: params.email })
-    if (pgUser !== undefined) {
-      return {
-        id: pgUser?.id.toString(),
-        name: pgUser.name ?? undefined
-      }
-    }
-  }
-}
+const makeFakeDb = async (entities?: any[]): Promise<IMemoryDb> => {
+  const db = newDb()
+  const connection = await db.adapters.createTypeormConnection({
+    type: 'postgres',
+    entities: entities ?? ['src/infra/postgres/entities/index.ts']
+  })
 
-@Entity({ name: 'usuarios' })
-class PgUser {
-  @PrimaryGeneratedColumn()
-    id!: number
-
-  @Column({ name: 'nome', nullable: true })
-    name?: string
-
-  @Column()
-    email!: string
-
-  @Column({ name: 'facebook_id', nullable: true })
-    facebookId?: string
+  await connection.synchronize()
+  return db
 }
 
 describe('PgUserAccountRepository', () => {
@@ -38,13 +22,7 @@ describe('PgUserAccountRepository', () => {
     let backup: IBackup
 
     beforeAll(async () => {
-      const db = newDb()
-      const connection = await db.adapters.createTypeormConnection({
-        type: 'postgres',
-        entities: [PgUser]
-      })
-
-      await connection.synchronize()
+      const db = await makeFakeDb([PgUser])
       backup = db.backup()
       pgUserRepo = getRepository(PgUser)
     })
