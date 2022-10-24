7831d1a33ebc996fbc256fc72f53a2e7dc539fa6
Roberto Kang <roberto.kang@bolttech.io>
2022-10-24 11:55:22 -0300

    test: ensure ExpressRouter calls handle with correct request

---
+++ tests/infra/http/express-router.spec.ts
@@ -0,0 +1,26 @@
+import { Controller } from '@/application/controllers'
+
+import { getMockReq, getMockRes } from '@jest-mock/express'
+import { Request, Response } from 'express'
+import { mock } from 'jest-mock-extended'
+
+class ExpressRouter {
+  constructor (private readonly controller: Controller) {}
+
+  async adapt (req: Request, res: Response): Promise<void> {
+    await this.controller.handle({ ...req.body })
+  }
+}
+
+describe('ExpressRouter', () => {
+  it('Should call handle with correct request', async () => {
+    const req = getMockReq({ body: { any: 'any' } })
+    const { res } = getMockRes()
+    const controller = mock<Controller>()
+    const sut = new ExpressRouter(controller)
+
+    await sut.adapt(req, res)
+
+    expect(controller.handle).toHaveBeenCalledWith({ any: 'any' })
+  })
+})
