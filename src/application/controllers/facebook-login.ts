709dcff97ef7e42b10cafa9ef801b60093f359a1
Roberto Kang <roberto.kang@bolttech.io>
2022-10-24 07:47:36 -0300

    feat: make FacebookLoginController use RequiredStringValidator

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -2,6 +2,7 @@ import { RequiredFieldError } from '@/application/errors'
 import { badRequest, HttpResponse, ok, serverError, unauthorized } from '@/application/helpers'
 import { IFacebookAuthentication } from '@/domain/features'
 import { AccessToken } from '@/domain/models'
+import { RequiredStringValidator } from '@/application/validation'
 
 type HttpRequest = {
   token: string
@@ -34,6 +35,7 @@ export class FacebookLoginController {
   }
 
   private validate (httpRequest: HttpRequest): Error | undefined {
-    if (httpRequest.token === '' || httpRequest.token === null || httpRequest.token === undefined) return new RequiredFieldError('token')
+    const validator = new RequiredStringValidator(httpRequest.token, 'token')
+    return validator.validate()
   }
 }
