ebf0e938deb2817107e20fd319afbc9891521c07
Roberto Kang <roberto.kang@bolttech.io>
2022-10-24 11:06:13 -0300

    refactor: move sut creation to beforeEach

--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -3,14 +3,19 @@ import { AxiosHttpClient } from '@/infra/http'
 import { env } from '@/main/config/env'
 
 describe('Facebook Api Integration Tests', () => {
-  it('Should return a Facebook user if token is valid', async () => {
-    const axiosClient = new AxiosHttpClient()
-    const sut = new FacebookApi(
+  let axiosClient: AxiosHttpClient
+  let sut: FacebookApi
+
+  beforeEach(() => {
+    axiosClient = new AxiosHttpClient()
+    sut = new FacebookApi(
       axiosClient,
       env.facebookApi.clientId,
       env.facebookApi.clientSecret
     )
+  })
 
+  it('Should return a Facebook user if token is valid', async () => {
     const fbUser = await sut.loadUser({ token: env.facebookApi.clientTestToken })
 
     expect(fbUser).toEqual({
@@ -21,13 +26,6 @@ describe('Facebook Api Integration Tests', () => {
   })
 
   it('Should return undefined if token is invalid', async () => {
-    const axiosClient = new AxiosHttpClient()
-    const sut = new FacebookApi(
-      axiosClient,
-      env.facebookApi.clientId,
-      env.facebookApi.clientSecret
-    )
-
     const fbUser = await sut.loadUser({ token: 'invalid' })
 
     expect(fbUser).toBeUndefined()
