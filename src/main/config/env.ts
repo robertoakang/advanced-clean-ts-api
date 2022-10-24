e2dd68a0d072440d8d035b2b61f981110713109c
Roberto Kang <roberto.kang@bolttech.io>
2022-10-24 11:30:29 -0300

    refactor: express config

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,5 +3,6 @@ export const env = {
     clientTestToken: 'EAAQwHlqGJGUBAEqaihbfl3gGeQCPUIPwIxTlI0CSEzAkEka14jmcAeMDrbJqFX0R84ZC6gL5SXeRuNXh3LSkD6S561BjHl9sjRfo877QVgvmuAtEB6u48159aTAPirLBMjZA8FWkQzWGa8tJoFOSCTZBJEZC1wYZCmYFUOfOmNv5UuHoGLBBATfZB9XYYhPVZBQMOYSZBZBWo780xetsaShvj',
     clientId: process.env.FB_CLIENT_ID ?? '1178806832735333',
     clientSecret: process.env.FB_CLIENT_SECRET ?? '83f5b1c310c0278315e3f0a12bb564c8'
-  }
+  },
+  port: process.env.PORT ?? '6969'
 }
