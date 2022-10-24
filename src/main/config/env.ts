8e7606a64b59e4bff82acab84137748cffff2341
Roberto Kang <roberto.kang@bolttech.io>
2022-10-24 11:03:27 -0300

    test: ensure Facebook Api returns Facebook user or undefined

---
+++ src/main/config/env.ts
@@ -0,0 +1,7 @@
+export const env = {
+  facebookApi: {
+    clientTestToken: 'EAAQwHlqGJGUBAEqaihbfl3gGeQCPUIPwIxTlI0CSEzAkEka14jmcAeMDrbJqFX0R84ZC6gL5SXeRuNXh3LSkD6S561BjHl9sjRfo877QVgvmuAtEB6u48159aTAPirLBMjZA8FWkQzWGa8tJoFOSCTZBJEZC1wYZCmYFUOfOmNv5UuHoGLBBATfZB9XYYhPVZBQMOYSZBZBWo780xetsaShvj',
+    clientId: process.env.FB_CLIENT_ID ?? '1178806832735333',
+    clientSecret: process.env.FB_CLIENT_SECRET ?? '83f5b1c310c0278315e3f0a12bb564c8'
+  }
+}
--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -1,5 +1,35 @@
-describe('', () => {
-  it('', () => {
+import { FacebookApi } from '@/infra/apis'
+import { AxiosHttpClient } from '@/infra/http'
+import { env } from '@/main/config/env'
 
+describe('Facebook Api Integration Tests', () => {
+  it('Should return a Facebook user if token is valid', async () => {
+    const axiosClient = new AxiosHttpClient()
+    const sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret
+    )
+
+    const fbUser = await sut.loadUser({ token: env.facebookApi.clientTestToken })
+
+    expect(fbUser).toEqual({
+      facebookId: '101969742721610',
+      name: 'Kang Teste',
+      email: 'kang_thbcfzk_teste@tfbnw.net'
+    })
+  })
+
+  it('Should return undefined if token is invalid', async () => {
+    const axiosClient = new AxiosHttpClient()
+    const sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret
+    )
+
+    const fbUser = await sut.loadUser({ token: 'invalid' })
+
+    expect(fbUser).toBeUndefined()
   })
 })
