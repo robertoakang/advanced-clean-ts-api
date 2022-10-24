80415f6d62a2adf57252247b049bde481c65b8dc
Roberto Kang <roberto.kang@bolttech.io>
2022-10-24 08:29:03 -0300

    test: ensure ValidationComposite returns the first error found

--- tests/application/validation/composite.spec.ts
+++ tests/application/validation/composite.spec.ts
@@ -4,11 +4,14 @@ interface IValidator {
   validate: () => Error | undefined
 }
 
-class ValidationComposite {
+class ValidationComposite implements IValidator {
   constructor (private readonly validators: IValidator[]) {}
 
-  validate (): undefined {
-    return undefined
+  validate (): Error | undefined {
+    for (const validator of this.validators) {
+      const error = validator.validate()
+      if (error !== undefined) return error
+    }
   }
 }
 
@@ -34,5 +37,13 @@ describe('ValidationComposite', () => {
     const error = sut.validate()
 
     expect(error).toBeUndefined()
+  })
+
+  it('Should return the first error', () => {
+    validator1.validate.mockReturnValueOnce(new Error('error_1'))
+    validator2.validate.mockReturnValueOnce(new Error('error_2'))
+    const error = sut.validate()
+
+    expect(error).toEqual(new Error('error_1'))
   })
 })
