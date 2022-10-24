55c2946d526dd5be119a41c585d5114e5d11ed1e
Roberto Kang <roberto.kang@bolttech.io>
2022-10-24 08:19:39 -0300

    refactor: isolate sut creation

--- tests/application/validation/composite.spec.ts
+++ tests/application/validation/composite.spec.ts
@@ -1,4 +1,4 @@
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
 
 interface IValidator {
   validate: () => Error | undefined
@@ -13,13 +13,24 @@ class ValidationComposite {
 }
 
 describe('ValidationComposite', () => {
-  it('Should return undefined if all validators returns undefined', () => {
-    const validator1 = mock<IValidator>()
-    validator1.validate.mockReturnValue(undefined)
-    const validator2 = mock<IValidator>()
-    validator2.validate.mockReturnValue(undefined)
-    const sut = new ValidationComposite([validator1, validator2])
+  let sut: ValidationComposite
+  let validator1: MockProxy<IValidator>
+  let validator2: MockProxy<IValidator>
+  let validators: IValidator[]
 
+  beforeAll(() => {
+    validator1 = mock()
+    validator1.validate.mockReturnValue(undefined)
+    validator2 = mock()
+    validator2.validate.mockReturnValue(undefined)
+    validators = [validator1, validator2]
+  })
+
+  beforeEach(() => {
+    sut = new ValidationComposite(validators)
+  })
+
+  it('Should return undefined if all validators returns undefined', () => {
     const error = sut.validate()
 
     expect(error).toBeUndefined()
