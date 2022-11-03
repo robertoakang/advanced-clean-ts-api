9273232dcbb57f44f3a84087dd12469560b930d8
Roberto Kang <roberto.kang@bolttech.io>
2022-11-03 18:40:30 -0300

    test: ensure JwtTokenHandler calls verify with correct params

--- src/infra/crypto/jwt-token-handler.ts
+++ src/infra/crypto/jwt-token-handler.ts
@@ -1,15 +1,16 @@
-import { ITokenGenerator } from '@/domain/contracts/crypto'
+import { ITokenGenerator, ITokenValidator } from '@/domain/contracts/crypto'
 
-import { sign } from 'jsonwebtoken'
-
-type Params = ITokenGenerator.Params
-type Result = ITokenGenerator.Result
+import { sign, verify } from 'jsonwebtoken'
 
 export class JwtTokenHandler implements ITokenGenerator {
   constructor (private readonly secret: string) {}
 
-  async generateToken ({ expirationInMs, key }: Params): Promise<Result> {
+  async generateToken ({ expirationInMs, key }: ITokenGenerator.Params): Promise<ITokenGenerator.Result> {
     const expirationInSeconds = expirationInMs / 1000
     return sign({ key }, this.secret, { expiresIn: expirationInSeconds })
+  }
+
+  async validateToken ({ token }: ITokenValidator.Params): Promise<void> {
+    verify(token, this.secret)
   }
 }
