8ec44e1f329c8bba9dc6674a0400d663cdcff283
Roberto Kang <roberto.kang@bolttech.io>
2022-11-08 18:09:46 -0300

    build: remove hardcoded sensitive data

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,9 +1,9 @@
 export const env = {
   facebookApi: {
-    clientTestToken: 'EAAQwHlqGJGUBAEqaihbfl3gGeQCPUIPwIxTlI0CSEzAkEka14jmcAeMDrbJqFX0R84ZC6gL5SXeRuNXh3LSkD6S561BjHl9sjRfo877QVgvmuAtEB6u48159aTAPirLBMjZA8FWkQzWGa8tJoFOSCTZBJEZC1wYZCmYFUOfOmNv5UuHoGLBBATfZB9XYYhPVZBQMOYSZBZBWo780xetsaShvj',
-    clientId: process.env.FB_CLIENT_ID ?? '1178806832735333',
-    clientSecret: process.env.FB_CLIENT_SECRET ?? '83f5b1c310c0278315e3f0a12bb564c8'
+    clientId: process.env.FB_CLIENT_ID ?? '',
+    clientSecret: process.env.FB_CLIENT_SECRET ?? '',
+    clientTestToken: process.env.FB_ACCESS_TOKEN ?? ''
   },
-  port: process.env.PORT ?? '6969',
-  jwtSecret: process.env.JWT_SECRET ?? '1o231099adsok'
+  port: process.env.PORT ?? 6969,
+  jwtSecret: process.env.JWT_SECRET ?? ''
 }
