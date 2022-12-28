718bec8a47d42ea9a28c75e0f809f4e27b7bcd81
Roberto Kang <roberto.kang@bolttech.io>
2022-12-28 02:34:14 +0000

    refactor: move production code to its own file

---
+++ src/application/validation/allowed-mime-types.ts
@@ -0,0 +1,24 @@
+import { InvalidMimeTypeError } from '@/application/errors'
+
+type Extension = 'png' | 'jpg'
+
+export class AllowedMimeTypes {
+  constructor (
+    private readonly allowed: Extension[],
+    private readonly mimeType: string
+  ) {}
+
+  validate (): Error | undefined {
+    let isValid = false
+    if (this.isPng() || this.isJpg()) isValid = true
+    if (!isValid) return new InvalidMimeTypeError(this.allowed)
+  }
+
+  private isPng (): boolean {
+    return this.allowed.includes('png') && this.mimeType === 'image/png'
+  }
+
+  private isJpg (): boolean {
+    return this.allowed.includes('jpg') && /image\/jpe?g/.test(this.mimeType)
+  }
+}
