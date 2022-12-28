6bdef7b037cd4607433be09858f5321cb6b95981
Roberto Kang <roberto.kang@bolttech.io>
2022-12-28 01:21:45 +0000

    test: ensure SavePictureController returns 400 if file is not provided

--- tests/application/controllers/save-picture.spec.ts
+++ tests/application/controllers/save-picture.spec.ts
@@ -12,11 +12,22 @@ export class SavePictureController {
 
 describe('SavePictureController', () => {
   let sut: SavePictureController
+
   beforeEach(() => {
     sut = new SavePictureController()
   })
+
   it('Should return 400 if file is not provided', async () => {
     const httpResponse = await sut.handle({ file: undefined })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new RequiredFieldError('file')
+    })
+  })
+
+  it('Should return 400 if file is not provided', async () => {
+    const httpResponse = await sut.handle({ file: null })
 
     expect(httpResponse).toEqual({
       statusCode: 400,
