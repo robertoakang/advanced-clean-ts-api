46488395661a0c8e31bb4bbceabfc8fbedfc426f
Roberto Kang <roberto.kang@bolttech.io>
2022-12-28 23:09:27 +0000

    test: ensure PgConnection is a Singleton

---
+++ tests/infra/repos/postgres/helpers/connection.spec.ts
@@ -0,0 +1,21 @@
+
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export class PgConnection {
+  private static instance?: PgConnection
+
+  private constructor () {}
+
+  static getInstance (): PgConnection {
+    if (PgConnection.instance === undefined) PgConnection.instance = new PgConnection()
+    return PgConnection.instance
+  }
+}
+
+describe('PgConnection', () => {
+  it('Should have only one instance - be a Singleton', () => {
+    const sut = PgConnection.getInstance()
+    const sut2 = PgConnection.getInstance()
+
+    expect(sut).toBe(sut2)
+  })
+})
