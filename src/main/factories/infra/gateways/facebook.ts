98afbde8187504c159c2b222eda3694717149ffe
Roberto Kang <roberto.kang@bolttech.io>
2022-12-28 00:10:31 +0000

    refactor: organize factories folder

--- src/main/factories/infra/gateways/facebook.ts
+++ src/main/factories/infra/gateways/facebook.ts
@@ -1,5 +1,5 @@
 import { FacebookApi } from '@/infra/gateways'
 import { env } from '@/main/config/env'
-import { makeAxiosHttpClient } from '@/main/factories/gateways'
+import { makeAxiosHttpClient } from '@/main/factories/infra/gateways'
 
 export const makeFacebookApi = (): FacebookApi => new FacebookApi(makeAxiosHttpClient(), env.facebookApi.clientId, env.facebookApi.clientSecret)
