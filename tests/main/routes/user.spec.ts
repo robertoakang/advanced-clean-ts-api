c81c939ad6b3e5da9528eace88cb466538f8dbd1
Roberto Kang <roberto.kang@bolttech.io>
2022-12-28 21:02:22 +0000

    test: ensure PUT /users/picture returns 200 with valid data

--- tests/main/routes/user.spec.ts
+++ tests/main/routes/user.spec.ts
@@ -43,19 +43,36 @@ describe('User Routes', () => {
         .set({ authorization })
 
       expect(status).toBe(200)
-      expect(body).toEqual({
-        pictureUrl: undefined,
-        initials: 'RK'
-      })
+      expect(body).toEqual({ pictureUrl: undefined, initials: 'RK' })
     })
   })
 
   describe('PUT /users/picture', () => {
+    const uploadSpy = jest.fn()
+
+    jest.mock('@/infra/gateways/aws-s3-file-storage', () => ({
+      AwsS3FileStorage: jest.fn().mockReturnValue({ upload: uploadSpy })
+    }))
+
     it('Should return 403 if not autherization header is present', async () => {
       const { status } = await request(app)
         .put('/api/users/picture')
 
       expect(status).toBe(403)
+    })
+
+    it('Should return 200 with valid data', async () => {
+      uploadSpy.mockResolvedValueOnce('any_url')
+      const { id } = await pgUserRepo.save({ email: 'any_email', name: 'Roberto Kang' })
+      const authorization = sign({ key: id }, env.jwtSecret)
+
+      const { status, body } = await request(app)
+        .put('/api/users/picture')
+        .set({ authorization })
+        .attach('picture', Buffer.from('any_buffer'), { filename: 'any_name', contentType: 'image/png' })
+
+      expect(status).toBe(200)
+      expect(body).toEqual({ pictureUrl: 'any_url', initials: undefined })
     })
   })
 })
