bcdaed763fdc1a857d7e22ac4b82960b35888a66
Roberto Kang <roberto.kang@bolttech.io>
2022-12-29 10:58:27 +0000

    feat: add PgConnectionController factory

--- src/application/errors/http.ts
+++ src/application/errors/http.ts
@@ -1,6 +1,6 @@
 export class ServerError extends Error {
   constructor (error?: Error) {
-    super('Server failed, Try again soon')
+    super('Server failed. Try again soon')
     this.name = 'ServerError'
     this.stack = error?.stack
   }
